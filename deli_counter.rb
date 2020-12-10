babd0001d3771a0ca4eafd80ae24fecd9e8fad81
Cassandra <cparisi1290@gmail.com>
2020-12-10 10:29:08 -0500

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(x)
+    line_array = []
+    if x.length == 0
+        puts "The line is currently empty."
+    else 
+        x.each.with_index(1) do |name, index|
+            line_array << "#{index}. #{name}"
+        end
+        puts "The line is currently: #{line_array.join(" ")}"
+    end
+end
+
+def take_a_number(katz_deli, name)
+    katz_deli << "#{name}"
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(n)
+    if n.length == 0
+        puts "There is nobody waiting to be served!"
+    else 
+        puts "Currently serving #{n[0]}."
+        n.shift
+    end
+end
